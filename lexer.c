133ba481b1709d1bac2fee0c93e5db385d6b83f0
Najib Amoussaoui <namoussa@c4r4p6.1337.ma>
2024-07-10 14:28:00 +0100

    lexer1

--- lexer.c
+++ lexer.c
@@ -8,29 +8,26 @@ int ft_sp(char c)
 int word_token(t_mini *prog, t_token **head, int i)
 {
     char *ptr;
-    int dflag = 0;
-    int sflag = 0;
-    int len = 0;
-
-    while(prog->line[i + len])
+    int dflag;
+    int sflag;
+    int len;
+    
+    dflag = 0;
+    sflag = 0;
+    len = -1;
+    while(prog->line[i + (++len)])
     {
         if(dflag == 0 && sflag == 0)
-          {
-            if(prog->line[i + len] && ft_sp(prog->line[i + len]) == 1)
-                break;
-          }
+                if(prog->line[i + len] && ft_sp(prog->line[i + len]) == 1)
+                      break;
         if( prog->line[i + len] == '\"' && sflag == 0)
                  dflag = !dflag;
-         if(  prog->line[i + len] == '\'' && dflag == 0)
-               sflag = !sflag;
-        len++;
+        if(  prog->line[i + len] == '\'' && dflag == 0)
+                 sflag = !sflag;
     }
     ptr = malloc(sizeof(char) * (len + 1));
     if (!ptr) 
-    {
-        perror("malloc");
         exit(1);
-    }
     ft_strncpy(ptr, prog->line + i, len + 1);
     ptr[len] = '\0';
     addback_node(head, create_newnode(TOKEN_WORD, ptr));
@@ -48,7 +45,6 @@ void ft_lexer(t_mini *prog, t_token **head)
               i++;
     if (prog->line[i] == '\0')
         break;
-    
     if (prog->line[i] == '|') 
             addback_node(head, create_newnode(TOKEN_PIPE, "|"));
     else if (prog->line[i] == '>' && prog->line[i + 1] == '>' && ++i) 
