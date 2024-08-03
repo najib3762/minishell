9edf4796c61f40f92fe72e050b6eb2afda574487
najib3762 <najibamoussaoui5@gmail.com>
2024-08-03 14:33:13 +0100

    unsetfree

--- Built_in/Built-in2.c
+++ Built_in/Built-in2.c
@@ -83,111 +83,116 @@ char	*g_env(t_list *env, char *str)
 	return (NULL);
 }
 
-void set_env_old(t_list **env, const char *name, t_list **export_list)
+void	set_env_old(t_list **env, const char *name, t_list **export_list)
 {
-    t_list *tmp;
-    size_t name_len = strlen(name);
-	char *pwd_content;
-	
+	t_list	*tmp;
+	size_t	name_len;
+	char	*pwd_content;
+
+	name_len = strlen(name);
 	tmp = *env;
-	while (tmp)	
+	while (tmp)
 	{
 		if (!ft_strncmp(tmp->content, "PWD=", 4))
-			pwd_content = m_substr(tmp->content, 4, ft_strlen(tmp->content) - 4);
+			pwd_content = m_substr(tmp->content, 4, ft_strlen(tmp->content)
+					- 4);
 		tmp = tmp->next;
 	}
 	tmp = *env;
-    while (tmp)
-    {
-        if (!ft_strncmp((char *)tmp->content, name, name_len) && ((char *)(tmp->content))[name_len] == '=')
-            tmp->content = m_strjoin("OLDPWD=", pwd_content);
-        tmp = tmp->next;
-    } 
+	while (tmp)
+	{
+		if (!ft_strncmp((char *)tmp->content, name, name_len)
+			&& ((char *)(tmp->content))[name_len] == '=')
+			tmp->content = m_strjoin("OLDPWD=", pwd_content);
+		tmp = tmp->next;
+	}
 	if (tmp)
 		ft_lstadd_back(env, m_lstnew(pwd_content));
 	tmp = *export_list;
 	while (tmp)
-    {
-        if (!ft_strncmp((char *)tmp->content, name, name_len) && ((char *)(tmp->content))[name_len] == '=')
-        {
-            tmp->content = m_strjoin("OLDPWD=", pwd_content);
+	{
+		if (!ft_strncmp((char *)tmp->content, name, name_len)
+			&& ((char *)(tmp->content))[name_len] == '=')
+		{
+			tmp->content = m_strjoin("OLDPWD=", pwd_content);
 			return ;
-        }
-        tmp = tmp->next;
-    }
-    ft_lstadd_back(export_list, m_lstnew(pwd_content));
+		}
+		tmp = tmp->next;
+	}
+	ft_lstadd_back(export_list, m_lstnew(pwd_content));
 }
 
-void set_env_pwd(t_list **env, t_list **export_list, char *new_pwd)
+void	set_env_pwd(t_list **env, t_list **export_list, char *new_pwd)
 {
-    t_list *tmp;
-    size_t name_len = strlen("PWD=");
-	
-    char *pwd_content;
+	t_list	*tmp;
+	size_t	name_len;
+	char	*pwd_content;
+
+	name_len = strlen("PWD=");
 	tmp = *env;
-	while (tmp)	
+	while (tmp)
 	{
 		if (!ft_strncmp(tmp->content, "PWD=", 4))
-			pwd_content = m_substr(tmp->content, 4, ft_strlen(tmp->content) - 4);
+			pwd_content = m_substr(tmp->content, 4, ft_strlen(tmp->content)
+					- 4);
 		tmp = tmp->next;
 	}
 	tmp = *env;
-    while (tmp)
-    {
-        if (strncmp(tmp->content, "PWD=", name_len) == 0)
-            tmp->content = m_strjoin("PWD=", new_pwd);
-        tmp = tmp->next;
-    }
-    if (tmp)
-        ft_lstadd_back(env, m_lstnew(new_pwd));
-    tmp = *export_list;
-    while (tmp)
-    {
-        if (ft_strncmp(tmp->content, "PWD=", name_len) == 0)
-        {
-            tmp->content = m_strjoin("PWD=", new_pwd);
-            return;
-        }
-        tmp = tmp->next;
-    }
-        ft_lstadd_back(export_list, m_lstnew(new_pwd));
+	while (tmp)
+	{
+		if (strncmp(tmp->content, "PWD=", name_len) == 0)
+			tmp->content = m_strjoin("PWD=", new_pwd);
+		tmp = tmp->next;
+	}
+	if (tmp)
+		ft_lstadd_back(env, m_lstnew(new_pwd));
+	tmp = *export_list;
+	while (tmp)
+	{
+		if (ft_strncmp(tmp->content, "PWD=", name_len) == 0)
+		{
+			tmp->content = m_strjoin("PWD=", new_pwd);
+			return ;
+		}
+		tmp = tmp->next;
+	}
+	ft_lstadd_back(export_list, m_lstnew(new_pwd));
 }
-void update_pwd_and_oldpwd(t_list **env, t_list **export_list)
+void	update_pwd_and_oldpwd(t_list **env, t_list **export_list)
 {
-    char *current_pwd = getcwd(NULL, 0);
+	char	*current_pwd;
 
-    
-    if (current_pwd)
-    {
-        set_env_old(env, "OLDPWD", export_list);
-        set_env_pwd(env, export_list, current_pwd);
-        free(current_pwd);
-    }
-    else
-    {
-        g_global->exit_status = 1;
-    }
+	current_pwd = getcwd(NULL, 0);
+	if (current_pwd)
+	{
+		set_env_old(env, "OLDPWD", export_list);
+		set_env_pwd(env, export_list, current_pwd);
+		free(current_pwd);
+	}
+	else
+	{
+		g_global->exit_status = 1;
+	}
 }
-void change_to_home(t_list **env)
+void	change_to_home(t_list **env)
 {
-    char *path = g_env(*env, "HOME=");
+	char	*path;
 
-    
-    if (path)
-    {
-        if (chdir(path + 5) < 0)
-        {
-            perror("cd");
-            g_global->exit_status = 1;
-        }
-    }
-    else
-    {
-        printf("cd: HOME not set\n");
-        g_global->exit_status = 1;
-    }
+	path = g_env(*env, "HOME=");
+	if (path)
+	{
+		if (chdir(path + 5) < 0)
+		{
+			perror("cd");
+			g_global->exit_status = 1;
+		}
+	}
+	else
+	{
+		ft_putendl_fd("cd: HOME not set", 2);
+		g_global->exit_status = 1;
+	}
 }
-
 
 int	ft_cd(t_parse *arg, t_list **env, t_list **export_list)
 {
@@ -210,8 +215,8 @@ int	ft_cd(t_parse *arg, t_list **env, t_list **export_list)
 	}
 	update_pwd_and_oldpwd(env, export_list);
 	if (g_global->exit_status != 0)
-        return 1;
-    return (g_global->exit_status = 0, 0);
+		return (1);
+	return (g_global->exit_status = 0, 0);
 }
 
 int	ft_unset(t_list **env, t_list **exp_list, t_parse *cmd)
@@ -248,7 +253,6 @@ void	set_unset(t_list **head, char *var_name)
 				*head = current->next;
 			else
 				prev->next = current->next;
-			free(current);
 			break ;
 		}
 		prev = current;
@@ -259,30 +263,26 @@ void	set_unset(t_list **head, char *var_name)
 int	ft_exit(t_parse *cmd, t_mini *prog)
 {
 	t_args	*cur;
-	int content;
+	int		content;
 
 	cur = cmd->cmd_args;
 	if (!ft_strncmp(cur->content, "exit", 5))
 		cur = cur->next;
 	if (my_lstsize(cmd->cmd_args) > 2 && ft_isnumeric(cur->content))
 		return (g_global->exit_status = 1,
-			ft_putendl_fd("exit: too many arguments", 2));
+				ft_putendl_fd("exit: too many arguments", 2));
 	if (cur && cur->content)
 	{
-			content = ft_atoi(cur->content);
-		if (ft_isnumeric(cur->content) && free_all(prog) && ft_putendl_fd("exit", 1))
+		content = ft_atoi(cur->content);
+		if (ft_isnumeric(cur->content) && free_all(prog)
+			&& ft_putendl_fd("exit", 1))
 			exit(content % 256);
-		else if (!ft_isnumeric(cur->content) && free_all(prog))
-		{
-			ft_putendl_fd("exit: numeric argument required", 2);
+		else if (!ft_isnumeric(cur->content) && free_all(prog)
+				&& ft_putendl_fd("exit: numeric argument required", 2))
 			exit(2);
-		} 
 	}
-	else
-	{
-		ft_putendl_fd("exit", 1);
-		free_all(prog);
+	else if ((!cur || !cur->content) && free_all(prog)
+		&& ft_putendl_fd("exit", 1))
 		exit(0);
-	}
 	return (0);
 }
